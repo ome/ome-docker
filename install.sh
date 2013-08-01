408dbd546592cbe0ce79f0441344a5faafbe6ac5
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2013-08-01 12:14:08 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+
+apt-get update
+
+apt-get install -y postgresql 
+su - postgres -c "createuser -D -R -S omero"
+su - postgres -c "createdb -O omero omero"
+sed -i "/127.0.0.1/s/md5/trust/" /etc/postgresql/9.1/main/pg_hba.conf
+/etc/init.d/postgresql reload
+psql -h localhost -U omero omero </tmp/OMERO4.4__0.sql
+
+useradd -m omero
+
+apt-get install -y wget unzip
+wget -q http://cvs.openmicroscopy.org.uk/snapshots/omero/4.4.8p1/OMERO.server-4.4.8p1-ice34-b304.zip -P /tmp
+su - omero -c "unzip /tmp/OMERO.server-4.4.8p1-ice34-b304.zip -d /home/omero/"
+ln -s /home/omero/OMERO.server-4.4.8p1-ice34-b304 /home/omero/OMERO.server
+
+apt-get install -y zeroc-ice34 openjdk-7-jre-headless
+update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+su - omero -c "mkdir /home/omero/OMERO"
+su - omero -c "/home/omero/OMERO.server/bin/omero config set omero.data.dir /home/omero/OMERO"
