25cd9776122fb87cf06d6dc580335ff978a03186
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2013-08-01 12:39:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ psql -h localhost -U omero omero </tmp/OMERO4.4__0.sql
 useradd -m omero
 
 apt-get install -y wget unzip
-su - omero -c "wget http://cvs.openmicroscopy.org.uk/snapshots/omero/4.4.8p1/OMERO.server-4.4.8p1-ice34-b304.zip -P /tmp"
-su - omero -c "unzip /tmp/OMERO.server-4.4.8p1-ice34-b304.zip -d /home/omero/"
-ln -s /home/omero/OMERO.server-4.4.8p1-ice34-b304 /home/omero/OMERO.server
+su - omero -c "wget http://cvs.openmicroscopy.org.uk/snapshots/omero/4.4.8p1/OMERO.server-4.4.8p1-ice34-b304.zip -P /home/omero/"
+su - omero -c "unzip /home/omero/OMERO.server-4.4.8p1-ice34-b304.zip -d /home/omero/"
+su - omero -c "ln -s /home/omero/OMERO.server-4.4.8p1-ice34-b304 /home/omero/OMERO.server"
 
 apt-get install -y zeroc-ice34 openjdk-7-jre-headless
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
