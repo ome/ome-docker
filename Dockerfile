629ef24a47850790dd32f56d554ab539ffe5489a
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2013-08-02 23:49:37 +0100

    Auto-generate SQL

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM ubuntu:12.04
 
 RUN echo "deb http://archive.ubuntu.com/ubuntu precise universe" >>/etc/apt/sources.list
+
 ADD . /tmp/docker/
 RUN sh -eu /tmp/docker/install.sh
 RUN rm -fr /tmp/docker/
 
 EXPOSE 4063:4063 4064:4064
 
-CMD ["sh", "-c", "/etc/init.d/postgresql start && echo 127.0.0.1 `hostname` >>/etc/hosts && su - omero -c '/home/omero/OMERO.server/bin/omero admin start' && tail -f /home/omero/OMERO.server/var/log/Blitz-0.log"]
+CMD ["sh", "-c", "echo 127.0.0.1 `hostname` >>/etc/hosts && /etc/init.d/postgresql start && su - omero -c /home/omero/OMERO.server/bin/omero"]
--- install.sh
+++ install.sh
@@ -2,14 +2,13 @@
 
 apt-get update
 
-apt-get install -y postgresql 
-su - postgres -c "createuser -D -R -S omero"
-su - postgres -c "createdb -O omero omero"
-sed -i "/127.0.0.1/s/md5/trust/" /etc/postgresql/9.1/main/pg_hba.conf
-/etc/init.d/postgresql reload
-psql -h localhost -U omero omero </tmp/docker/OMERO4.4__0.sql
+apt-get install -y postgresql
+su - postgres -c "echo create user ${DB_USER-omero} password \'${DB_PASS-omero}\' | psql"
+su - postgres -c "createdb -O ${DB_USER-omero} ${DB_NAME-omero}"
 
 useradd -m omero
+mkdir /OMERO
+chown omero /OMERO
 
 apt-get install -y wget unzip
 su - omero -c "wget http://cvs.openmicroscopy.org.uk/snapshots/omero/4.4.8p1/OMERO.server-4.4.8p1-ice34-b304.zip -P /tmp/"
@@ -19,5 +18,5 @@ su - omero -c "ln -s /home/omero/OMERO.server-4.4.8p1-ice34-b304 /home/omero/OME
 
 apt-get install -y zeroc-ice34 openjdk-7-jre-headless
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
-su - omero -c "mkdir /home/omero/OMERO"
-su - omero -c "/home/omero/OMERO.server/bin/omero config set omero.data.dir /home/omero/OMERO"
+
+su - omero -c "/home/omero/OMERO.server/bin/omero db script -f - OMERO4.4 0 ${ROOT_PASS-omero}" | PGPASSWORD=${DB_PASS-omero} psql -h ${DB_HOST-localhost} -U ${DB_USER-omero} ${DB_NAME-omero}
