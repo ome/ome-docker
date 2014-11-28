26099273e563f6e4f63380b43275e593ad4059c5
Simon Li <spli@dundee.ac.uk>
2014-11-28 12:18:59 +0000

    Tarantula requires SUPER priv at runtime :-(

--- tarantula/mysql-postinstall.sql
+++
@@ -1,3 +0,0 @@
-REVOKE SUPER ON *.* FROM 'tarantula'@'localhost';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root_password');
-SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('root_password');
--- tarantula/mysql-preinstall.sql
+++ tarantula/mysql-preinstall.sql
@@ -3,5 +3,7 @@ DELETE FROM mysql.db WHERE db LIKE 'test%';
 -- Tarantula install will create a database, so just grant privileges
 -- CREATE DATABASE tarantula DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 GRANT ALL ON tarantula.* TO 'tarantula'@'localhost' IDENTIFIED BY 'tarantula';
--- Tarantula install requires SUPER privilege
+-- Tarantula install and runtime requires SUPER privilege
 GRANT SUPER ON *.* TO 'tarantula'@'localhost';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('root_password');
+SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('root_password');
