da5966116aba12effdf2dee0828ace1ceab402b5
Simon Li <spli@dundee.ac.uk>
2015-05-13 14:10:40 +0100

    Add run/sh as entrypooint
    Moved all supervisor processes into separate files (to make it easier to disable individual processes in future). Added a default server (currently hardcoded until omego can automatically install the latest release).

---
+++ omero-server-prereq/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+if [ $# -gt 0 -a -n "$*" ]; then
+	OMEGO_ARGS="$@"
+	export OMEGO_ARGS
+else
+	mv /etc/supervisor/conf.d/omero.conf /etc/supervisor/conf.d/omero.disabled
+fi
+
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
