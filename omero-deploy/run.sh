9a0e4d8e66155f2239fb03bb134b57ffe97aaca5
Simon Li <spli@dundee.ac.uk>
2015-09-25 15:42:28 +0100

    Change system and omero passwords to $OMERO_PASSWORD if given

--- omero-deploy/run.sh
+++ omero-deploy/run.sh
@@ -2,12 +2,18 @@
 
 set -e
 
+if [ -n "$OMERO_PASSWORD" ]; then
+	echo "Setting omero password"
+	echo "omero:$OMERO_PASSWORD" | chpasswd
+	OMEGO_ARGS="--rootpass \"$OMERO_PASSWORD\""
+fi
+
 if [ $# -gt 0 -a -n "$*" ]; then
-	OMEGO_ARGS="$@"
-	export OMEGO_ARGS
+	OMEGO_ARGS="$OMEGO_ARGS $@"
 else
 	mv /etc/supervisor/conf.d/omero.conf /etc/supervisor/conf.d/omero.disabled
 fi
+export OMEGO_ARGS
 
 mkdir -p /data/supervisor
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
