f4cce02f55f9a44b3b792d94f8c643fb22068fd7
Simon Li <spli@dundee.ac.uk>
2015-11-22 18:38:39 +0000

    Update to 5.2.0, start icegridnode in the foreground

--- omero-grid/omero/run.sh
+++ omero-grid/omero/run.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
 
-env
+set -eu
 
-export HOME=/home/omero
-cd $HOME
-omero=$HOME/OMERO.server/bin/omero
+TARGET=$1
 
-export PGPASSWORD=omero
-psql -w -h $DB_PORT_5432_TCP_ADDR -Uomero -c "select * from dbpatch" 2> /dev/null || {
-    echo "Initialising database"
-    $omero db script -f omero.sql "" "" omero
-    psql -w -h $DB_PORT_5432_TCP_ADDR -Uomero -f omero.sql
-}
+OMERO_SERVER=/home/omero/OMERO.server
+omero=$OMERO_SERVER/bin/omero
 
-$omero config set omero.db.host $DB_PORT_5432_TCP_ADDR
-$omero config set omero.data.dir $OMERODATA
-$omero admin start
+if [ "$TARGET" = master ]; then
+    DBHOST=$DB_PORT_5432_TCP_ADDR
+    DBUSER=${DBUSER:-omero}
+    DBNAME=${DBNAME:-omero}
+    DBPASS=${DBPASS:-omero}
+    MASTER_IP=$(hostname -I)
 
-# We need a command that won't exit to keep the container running
-tail -F $HOME/OMERO.server/var/log/Blitz-0.log
+    export PGPASSWORD="$DBPASS"
+    psql -w -h $DBHOST -U$DBUSER $DBNAME -c \
+        "select * from dbpatch" 2> /dev/null && {
+        echo "Upgrading database"
+        DBCMD=upgrade
+    } || {
+        echo "Initialising database"
+        DBCMD=init
+    }
+    omego db $DBCMD --dbhost "$DBHOST" --dbuser "$DBUSER" --dbname "$DBNAME" \
+        --dbpass "$DBPASS" --serverdir=OMERO.server
+
+    $omero config set omero.db.host "$DBHOST"
+    $omero config set omero.db.user "$DBUSER"
+    $omero config set omero.db.name "$DBNAME"
+    $omero config set omero.db.pass "$DBPASS"
+
+    $omero config set Ice.Default.Host "$MASTER_IP"
+
+    exec $omero admin start --foreground
+fi
+
+if [ "$TARGET" = slave ]; then
+    MASTER_IP=$MASTER_PORT_4061_TCP_ADDR
+    SLAVE_IP=$(hostname -I)
+
+    $omero config set Ice.Default.Host "$MASTER_IP"
+
+    # TODO: `omero node start` doesn't rewrite the config
+    $omero admin rewrite
+    sed -i "s/@omero.slave.host@/$SLAVE_IP/" OMERO.server/etc/slave.cfg
+    grep '^Ice.Default.Router=' OMERO.server/etc/ice.config || \
+        echo Ice.Default.Router= >> OMERO.server/etc/ice.config
+    sed -i -r "s|^(Ice.Default.Router=).*|\1OMERO.Glacier2/router:tcp -p 4063 -h $MASTER_IP|" \
+        OMERO.server/etc/ice.config
+
+    # TODO: `omero node start` doesn't support --foreground
+    #exec $omero node slave start
+    cd $OMERO_SERVER
+    mkdir -p var/log var/slave
+    exec icegridnode --Ice.Config=$OMERO_SERVER/etc/internal.cfg,$OMERO_SERVER/etc/slave.cfg \
+        --nochdir
+fi
