2ff4b992386ddd4c699068db9edf1292a1ebffd3
Simon Li <spli@dundee.ac.uk>
2016-01-15 12:31:26 +0000

    Allow env-var MASTER_ADDR as an alternative to link for web

--- omero-grid/omero-grid-web/run.sh
+++ omero-grid/omero-grid-web/run.sh
@@ -4,12 +4,15 @@ set -eu
 
 omero=/home/omero/OMERO.py/bin/omero
 
-MASTER_IP=${MASTER_PORT_4064_TCP_ADDR:-}
-if [ -n "$MASTER_IP" ]; then
-    $omero config set omero.web.server_list \
-        "[[\"$MASTER_IP\", 4064, \"omero\"]]"
+MASTER_ADDR=${MASTER_ADDR:-}
+if [ -z "$MASTER_ADDR" ]; then
+    MASTER_ADDR=${MASTER_PORT_4064_TCP_ADDR:-}
+fi
+if [ -n "$MASTER_ADDR" ]; then
+    $omero config set omero.web.server_list "[[\"$MASTER_ADDR\", 4064, \"omero\"]]"
 else
-    echo "WARNING: Master IP not found"
+    echo "WARNING: Master address not found"
+    # Assume it'll be set in /config/*
 fi
 
 if stat -t /config/* > /dev/null 2>&1; then
