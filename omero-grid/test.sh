80709355ce676eeaf98b61b9e50f6044bac5ec1c
Simon Li <spli@dundee.ac.uk>
2016-01-27 08:51:01 +0000

    Add params to test.sh to make manual testing easier

--- omero-grid/test.sh
+++ omero-grid/test.sh
@@ -4,17 +4,33 @@ set -e
 set -u
 set -x
 
-cd omero-grid
+PREFIX=omero-grid-test
+IMAGE=omero-grid:$PREFIX
+IMAGEWEB=omero-grid-web:$PREFIX
+#BUILD="--release OMERO-DEV-merge-build"
+BUILD=
+
+#CLEAN=n
+CLEAN=y
 
 cleanup() {
-    docker rm -f postgres # In case other tests use the name
+    docker rm -f $PREFIX-db $PREFIX-master $PREFIX-web
 }
 
-trap cleanup ERR EXIT
+if [ "$CLEAN" = y ]; then
+    trap cleanup ERR EXIT
+fi
+
+cleanup || true
 
 # From the README
-./build.py omero-grid
-./build.py omero-grid-web
-docker run -d --name postgres -e POSTGRES_PASSWORD=postgres postgres
-docker run -d --name omero-master --link postgres:db -e DBUSER=postgres -e DBPASS=postgres -e DBNAME=postgres openmicroscopy/omero-grid master
+./build.py --tag $IMAGE $BUILD omero-grid
+./build.py --tag $IMAGEWEB $BUILD omero-grid-web
+docker run -d --name $PREFIX-db -e POSTGRES_PASSWORD=postgres postgres
+docker run -d --name $PREFIX-master --link $PREFIX-db:db -e DBUSER=postgres -e DBPASS=postgres -e DBNAME=postgres $IMAGE master
+docker run -d --name $PREFIX-web --link $PREFIX-master:master -P $IMAGEWEB
+
+echo "Exposed web port:"
+docker port $PREFIX-web
+
 # TBD: this doesn't yet check that they all come up.
