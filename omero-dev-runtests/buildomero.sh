04d718f9790a4f382d4a652d79db2290bfda27ba
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-02-28 23:38:56 +0000

    moving config to the separate file

--- omero-dev-runtests/buildomero.sh
+++ omero-dev-runtests/buildomero.sh
@@ -39,15 +39,3 @@ mv ~omero/$OMERO_ZIP ~omero/omero-source && rm $OMERO_ZIP.zip
 /home/omero/omero-source/build.py build-default test-compile
 
 ln -s /home/omero/omero-source/dist /home/omero/OMERO.server
-
-source /home/omero/settings.env
-
-sudo mkdir -p "$OMERO_DATA_DIR"
-sudo chown omero "$OMERO_DATA_DIR"
-
-/home/omero/OMERO.server/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
-/home/omero/OMERO.server/bin/omero config set omero.db.name "$OMERO_DB_NAME"
-/home/omero/OMERO.server/bin/omero config set omero.db.user "$OMERO_DB_USER"
-/home/omero/OMERO.server/bin/omero config set omero.db.pass "$OMERO_DB_PASS"
-
-/home/omero/OMERO.server/bin/omero db script -f OMERO.server/db.sql --password "$OMERO_ROOT_PASS"
