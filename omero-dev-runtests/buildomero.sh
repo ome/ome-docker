7af16624c5e42686fb2ffbffcf678fdd65e40e73
Aleksandra Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-03-10 20:49:28 +0000

    update github example

--- omero-dev-runtests/buildomero.sh
+++ omero-dev-runtests/buildomero.sh
@@ -18,18 +18,18 @@ pip install --upgrade -r /home/omero/requirements-py27.txt
 
 ## download source
 
-# from github curl -o ~omero/openmicroscopy_src.zip 
-# URL=https://github.com/openmicroscopy/openmicroscopy/archive/develop.zip 
+## from github
+# URL=https://github.com/openmicroscopy/openmicroscopy/archive/develop.zip
 # OMERO_ZIP=`echo "$URL" | rev | cut -d / -f 1 | rev`
-# OMERO_ZIP="${OMERO_ZIP%.*}"
-# curl -o ~omero/$OMERO_ZIP.zip $URL && \
-#     unzip -d /home/omero $OMERO_ZIP.zip 
+# OMERO_ZIP=openmicroscopy-$OMERO_ZIP
+# curl -LkSs $URL -o ~omero/$OMERO_ZIP && ls -al && \
+#     unzip -d /home/omero $OMERO_ZIP
 
 # using omego
 omego download --branch $OMERO_BRANCH openmicroscopy
+
 OMERO_ZIP=`ls | grep openmicroscopy*.zip`
 OMERO_ZIP="${OMERO_ZIP%.*}"
-
 
 
 mv ~omero/$OMERO_ZIP ~omero/omero-source && rm $OMERO_ZIP.zip
