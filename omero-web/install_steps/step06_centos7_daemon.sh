3f9ffe8045852ae9bc9b5c5f4b868b5b3fa0aad8
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-03-18 15:52:30 +0000

    cleaning up files

--- omero-web/install_steps/step06_centos7_daemon.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp omero-web-systemd.service /etc/systemd/system/omero-web.service
-ln -s /etc/systemd/system/omeroweb.service /etc/systemd/system/multi-user.target.wants/omero-web.service
--- omero-web/omero-install.sh
+++ omero-web/omero-install.sh
@@ -4,8 +4,14 @@
 
 set -eux
 
-git clone -b java-parameters https://github.com/aleksandra-tarkowska/omero-install.git /tmp/omero-install
+git clone -b java-parameters https://github.com/jburel/omero-install.git /tmp/omero-install
 
 # delete invalid files
 rm -f /tmp/omero-install/linux/setup_centos_selinux.sh
 touch /tmp/omero-install/linux/setup_centos_selinux.sh
+
+# remove systemctl lines
+sed -i.bak -re 's/(systemctl.*)/ #\1/' /tmp/omero-install/linux/step05_centos7_nginx.sh
+
+sed -i.bak -re 's/(.*omero.service.*)/ #\1/' /tmp/omero-install/linux/step06_centos7_daemon.sh
+sed -i.bak -re 's/(systemctl daemon-reload)/ #\1/' /tmp/omero-install/linux/step06_centos7_daemon.sh
