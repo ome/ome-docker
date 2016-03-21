219fbba4e82922cb1d0bb51f2ab9a1c67ff81362
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-03-21 13:40:17 +0000

    cleaning up postgres and other files

--- omero-dev-runtests/install_steps/step04_all_omero.sh
+++ omero-dev-runtests/install_steps/step04_all_omero.sh
@@ -51,9 +51,6 @@ sudo mkdir -p "$OMERO_DATA_DIR"
 sudo chown omero "$OMERO_DATA_DIR"
 
 # load omero config
-/home/omero/OMERO.server/bin/omero config set omero.data.dir "$OMERO_DATA_DIR"
-/home/omero/OMERO.server/bin/omero config set omero.db.name "$OMERO_DB_NAME"
-/home/omero/OMERO.server/bin/omero config set omero.db.user "$OMERO_DB_USER"
-/home/omero/OMERO.server/bin/omero config set omero.db.pass "$OMERO_DB_PASS"
+source /home/omero/omero.config.sh
 
 /home/omero/OMERO.server/bin/omero db script -f OMERO.server/db.sql --password "$OMERO_ROOT_PASS"
--- omero-dev-runtests/omero-install.sh
+++ omero-dev-runtests/omero-install.sh
@@ -10,14 +10,25 @@ git clone -b java-parameters https://github.com/jburel/omero-install.git /tmp/om
 rm -f /tmp/omero-install/linux/setup_centos_selinux.sh
 touch /tmp/omero-install/linux/setup_centos_selinux.sh
 
-mv /tmp/omero-install/linux/step03_all_postgres.sh /home/omero/createdb.sh
-sed -i 's/source settings.env/source \/home\/omero\/settings.env/g' /home/omero/createdb.sh
-sed -i '$apsql -h localhost -U "$OMERO_DB_USER" "$OMERO_DB_NAME" < /home/omero/OMERO.server/db.sql' /home/omero/createdb.sh
+mv /tmp/omero-install/linux/step03_all_postgres.sh /home/omero/_step03_all_postgres.sh
+sed -i 's/source settings.env/source \/home\/omero\/settings.env/g' /home/omero/_step03_all_postgres.sh
+sed -i '$apsql -h localhost -U "$OMERO_DB_USER" "$OMERO_DB_NAME" < /home/omero/OMERO.server/db.sql' /home/omero/_step03_all_postgres.sh
 
-chown omero:omero /home/omero/createdb.sh
+chown omero:omero /home/omero/_step03_all_postgres.sh
 touch /tmp/omero-install/linux/step03_all_postgres.sh
 
-# remove systemctl lines
-sed -i.bak -re 's/(systemctl.*)/ #\1/' /tmp/omero-install/linux/step05_centos7_nginx.sh
+# replace initdb and remove systemctl
+sed -i.bak -re 's/(PGSETUP_INITDB_OPTIONS=.*)/#\1/' /tmp/omero-install/linux/step01_centos7_pg_deps.sh
 
-sed -i.bak -re 's/(systemctl daemon-reload)/ #\1/' /tmp/omero-install/linux/step06_centos7_daemon.sh
+sed -i.bak -re "/(PGSETUP_INITDB_OPTIONS.*-9\.4.*)/a\        su - postgres -c \"/usr/pgsql-9.4/bin/initdb -D /var/lib/pgsql/9.4/data --encoding=UTF8\"\n        echo \"listen_addresses=\'*\'\" >> /var/lib/pgsql/9.4/data/postgresql.conf" /tmp/omero-install/linux/step01_centos7_pg_deps.sh
+
+sed -i.bak -re "/(PGSETUP_INITDB_OPTIONS.*-9\.5.*)/a\        su - postgres -c \"/usr/pgsql-9.5/bin/initdb -D /var/lib/pgsql/9.5/data --encoding=UTF8\"\n        echo \"listen_addresses=\'*\'\" >> /var/lib/pgsql/9.5/data/postgresql.conf" /tmp/omero-install/linux/step01_centos7_pg_deps.sh
+
+
+sed -i.bak -re 's/(systemctl start.*)/#\1/' /tmp/omero-install/linux/step01_centos7_pg_deps.sh
+sed -i.bak -re 's/(systemctl daemon-reload)/#\1/' /tmp/omero-install/linux/step01_centos7_pg_deps.sh
+
+
+sed -i.bak -re 's/(systemctl start.*)/#\1/' /tmp/omero-install/linux/step05_centos7_nginx.sh
+
+sed -i.bak -re 's/(systemctl daemon-reload)/#\1/' /tmp/omero-install/linux/step06_centos7_daemon.sh
